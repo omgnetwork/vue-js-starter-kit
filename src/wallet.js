58ce79cac558c874da20af839fe2b7d44129cc8f
Kevin Sullivan <kevsul@gmail.com>
2019-02-25 13:10:48 +0100

    Add logs, better error handling

--- src/wallet.js
+++ src/wallet.js
@@ -6,7 +6,6 @@ const localConfig = {
   plasmaContractAddress: '0x9ea1e52e0e4ef1bf31a09fbf477f2aedf07bf64d'
 }
 
-
 const config = localConfig
 
 const erc20MinimalAbi = [
@@ -61,6 +60,13 @@ const erc20MinimalAbi = [
   }
 ]
 
+var web3 = new Web3()
+let globalKeystore
+let rootChain
+let childChain
+
+const MAX_LOGS = 5
+
 Vue.component('modal', {
   template: '#modal-template'
 })
@@ -81,17 +87,43 @@ const vm = new Vue({
     isShowExit: false,
     isShowTransfer: false,
     utxos: [],
-    utxoToExit: ''
+    utxoToExit: '',
+    logs: []
   },
   methods: {
-    restoreSeed: function () {
+    log: function (message) {
+      if (this.logs.length >= MAX_LOGS) {
+        this.logs.shift()
+      }
+      this.logs.push(message)
+    },
+    info: function (message) {
+      this.log({ message, level: 'info' })
+    },
+    error: function (message) {
+      this.log({ message, level: 'error' })
+    },
+
+    removeLog: function (log) {
+      this.logs = this.logs.filter(item => item !== log)
+    },
+
+    restoreSeed: async function () {
       var password = prompt('Enter Password to encrypt your seed', 'Password')
-      createVault(password, this.seed)
-      this.loadWallet = false
+      try {
+        const addresses = await createVault(password, this.seed)
+
+        this.accounts = addresses.map(address => ({ address, rootBalance: 0, childBalance: 0 }))
+        this.activeAccount = this.accounts[0]
+        this.refresh()
+        this.loadWallet = false
+      } catch (err) {
+        this.error(err)
+      }
     },
 
     refresh: function () {
-      showBalance(this.activeAccount)
+      getBalances(this.activeAccount)
       this.getUtxos()
     },
 
@@ -102,34 +134,40 @@ const vm = new Vue({
 
       // Create the deposit transaction
       const depositTx = OmgUtil.transaction.encodeDeposit(from, value, tokenContract)
-      console.log(depositTx)
 
       if (tokenContract === OmgUtil.transaction.ETH_CURRENCY) {
-        rootChain.depositEth(depositTx, value, { from })
-          .then(txhash => {
-            console.log('txhash: ' + txhash.transactionHash)
-          })
-          .catch(console.error)
+        try {
+          const tx = await rootChain.depositEth(depositTx, value, { from })
+          this.info(`Deposited ${value} ETH: ${tx.transactionHash}`)
+        } catch (err) {
+          this.error(err)
+        }
       } else {
         if (this.approveDeposit) {
           this.approveDeposit = false
           // First approve the plasma contract on the erc20 contract
           const erc20 = web3.eth.contract(erc20MinimalAbi).at(tokenContract)
-          const sendPromise = Promise.promisify(erc20.approve.sendTransaction)
+          const approvePromise = Promise.promisify(erc20.approve.sendTransaction)
 
           try {
-            const tx = await sendPromise(config.plasmaContractAddress, value, { from, gas: 200000 })
-            console.log(`${value} erc20 approved, tx: ${tx}`)
+            const gasPrice = web3.eth.gasPrice
+            const tx = await approvePromise(config.plasmaContractAddress, value, { from, gasPrice, gas: 2000000 })
+            // Wait for the approve tx to be mined
+            this.info(`${value} erc20 approved: ${tx}. Waiting for confirmation...`)
+            await awaitTx(web3, tx)
+            this.info(`... ${tx} confirmed.`)
           } catch (err) {
-            console.error(err)
+            this.error(err)
             return
           }
         }
 
-        rootChain.depositToken(depositTx, { from })
-          .then(txhash => {
-            console.log('txhash: ' + txhash.transactionHash)
-          })
+        try {
+          const tx = await rootChain.depositToken(depositTx, { from })
+          this.info(`Deposited ${value} ${tokenContract} tokens: ${tx.transactionHash}`)
+        } catch (err) {
+          this.error(err)
+        }
       }
     },
 
@@ -165,44 +203,56 @@ const vm = new Vue({
         })
       }
 
-      // Create the unsigned transaction
-      const unsignedTx = childChain.createTransaction(txBody)
+      try {
+        // Create the unsigned transaction
+        const unsignedTx = childChain.createTransaction(txBody)
 
-      const password = prompt('Enter password', 'Password')
+        const password = prompt('Enter password', 'Password')
 
-      // Sign it
-      globalKeystore.keyFromPassword(password, async function (err, pwDerivedKey) {
-        if (err) {
-          console.error(err)
-          return
-        }
-        // Decrypt the private key
-        const privateKey = globalKeystore.exportPrivateKey(fromAddr, pwDerivedKey)
-        // Sign the transaction with the private key
-        const signatures = await childChain.signTransaction(unsignedTx, [privateKey])
-        // Build the signed transaction
-        const signedTx = await childChain.buildSignedTransaction(unsignedTx, signatures)
-        // Submit the signed transaction to the childchain
-        const result = await childChain.submitTransaction(signedTx)
-        console.log(`Submitted transaction: ${JSON.stringify(result)}`)
-      })
+        // Sign it
+        globalKeystore.keyFromPassword(password, async (err, pwDerivedKey) => {
+          if (err) {
+            this.error(err)
+            return
+          }
+          try {
+            // Decrypt the private key
+            const privateKey = globalKeystore.exportPrivateKey(fromAddr, pwDerivedKey)
+            // Sign the transaction with the private key
+            const signatures = await childChain.signTransaction(unsignedTx, [privateKey])
+            // Build the signed transaction
+            const signedTx = await childChain.buildSignedTransaction(unsignedTx, signatures)
+            // Submit the signed transaction to the childchain
+            const result = await childChain.submitTransaction(signedTx)
+            this.info(`Submitted transaction: ${JSON.stringify(result)}`)
+          } catch (err) {
+            this.error(err)
+          }
+        })
+      } catch (err) {
+        this.error(err)
+      }
     },
 
     exit: async function () {
       const fromAddr = this.activeAccount.address
       const utxoToExit = this.utxoToExit
-      const exitData = await childChain.getExitData(utxoToExit)
+      try {
+        const exitData = await childChain.getExitData(utxoToExit)
 
-      let receipt = await rootChain.startStandardExit(
-        exitData.utxo_pos,
-        exitData.txbytes,
-        exitData.proof,
-        {
-          from: fromAddr
-        }
-      )
-      this.utxoToExit = ''
-      console.log(`RootChain.startExit(): txhash = ${receipt.transactionHash}`)
+        let receipt = await rootChain.startStandardExit(
+          exitData.utxo_pos,
+          exitData.txbytes,
+          exitData.proof,
+          {
+            from: fromAddr
+          }
+        )
+        this.utxoToExit = ''
+        this.info(`Called RootChain.startExit(): ${receipt.transactionHash}`)
+      } catch (err) {
+        this.error(err)
+      }
     },
 
     getUtxos: async function () {
@@ -221,73 +271,66 @@ const vm = new Vue({
   }
 })
 
-var web3 = new Web3()
-let globalKeystore
-let rootChain
-let childChain
-
 function createVault (password, seed) {
-  lightwallet.keystore.createVault({
-    password: password,
-    seedPhrase: seed,
-    hdPathString: "m/0'/0'/0'"
-  }, (err, keystore) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-
-    globalKeystore = keystore
-
-    const web3Provider = new HookedWeb3Provider({
-      host: config.web3ProviderUrl,
-      transaction_signer: globalKeystore
-    })
-    web3.setProvider(web3Provider)
-
-    rootChain = new RootChain(web3Provider, config.plasmaContractAddress)
-    childChain = new ChildChain(config.watcherUrl, config.childchainUrl)
-
-    globalKeystore.keyFromPassword(password, (err, pwDerivedKey) => {
+  return new Promise((resolve, reject) => {
+    lightwallet.keystore.createVault({
+      password: password,
+      seedPhrase: seed,
+      hdPathString: "m/0'/0'/0'"
+    }, (err, keystore) => {
       if (err) {
-        console.error(err)
+        reject(err)
         return
       }
 
-      // Show the first 2 addresses in the wallet
-      globalKeystore.generateNewAddress(pwDerivedKey, 1)
-      const addresses = globalKeystore.getAddresses()
-      vm.accounts = addresses.map(address => ({ address, rootBalance: 0, childBalance: 0 }))
-      vm.activeAccount = vm.accounts[0]
-      showBalance(vm.activeAccount)
-      vm.getUtxos()
+      globalKeystore = keystore
+
+      const web3Provider = new HookedWeb3Provider({
+        host: config.web3ProviderUrl,
+        transaction_signer: globalKeystore
+      })
+      web3.setProvider(web3Provider)
+
+      rootChain = new RootChain(web3Provider, config.plasmaContractAddress)
+      childChain = new ChildChain(config.watcherUrl, config.childchainUrl)
+
+      globalKeystore.keyFromPassword(password, (err, pwDerivedKey) => {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        // Get the first address in the wallet
+        globalKeystore.generateNewAddress(pwDerivedKey, 1)
+        const addresses = globalKeystore.getAddresses()
+        resolve(addresses)
+      })
     })
   })
 }
 
-async function showBalance (account) {
+async function getBalances (account) {
   web3.eth.getBalance(account.address, (err, ethBalance) => {
     if (err) {
-      console.error(err)
+      vm.error(err)
       return
     }
     account.rootBalance = ethBalance
   })
-  childChain.getBalance(account.address).then(childchainBalance => {
-    account.childBalance = childchainBalance
-    account.childBalance.forEach(balance => {
-      if (balance.currency === OmgUtil.transaction.ETH_CURRENCY) {
-        balance.symbol = 'ETH'
-      } else {
-        const tokenContract = web3.eth.contract(erc20MinimalAbi).at(balance.currency)
-        try {
-          const tokenSymbol = tokenContract.symbol()
-          balance.symbol = tokenSymbol
-        } catch (err) {
-          // console.error(err)
-        }
+
+  const childchainBalance = await childChain.getBalance(account.address)
+  account.childBalance = childchainBalance
+  account.childBalance.forEach(balance => {
+    if (balance.currency === OmgUtil.transaction.ETH_CURRENCY) {
+      balance.symbol = 'ETH'
+    } else {
+      const tokenContract = web3.eth.contract(erc20MinimalAbi).at(balance.currency)
+      try {
+        const tokenSymbol = tokenContract.symbol()
+        balance.symbol = tokenSymbol
+      } catch (err) {
       }
-    })
+    }
   })
 }
 
@@ -300,21 +343,61 @@ function selectUtxos (utxos, amount, currency) {
   }
 }
 
-async function startStandardExit () {
-  var fromAddr = document.getElementById('exitFromAddress').value
+const DEFAULT_INTERVAL = 500
+const DEFAULT_BLOCKS_TO_WAIT = 1
 
-  const utxos = await childChain.getUtxos(fromAddr)
-  if (utxos.length > 0) {
-    const exitData = await childChain.getExitData(utxos[0])
-
-    let receipt = await rootChain.startStandardExit(
-      exitData.utxo_pos,
-      exitData.txbytes,
-      exitData.proof,
-      {
-        from: fromAddr
+function awaitTx (web3, txnHash, options) {
+  const interval = options && options.interval ? options.interval : DEFAULT_INTERVAL
+  const blocksToWait = options && options.blocksToWait ? options.blocksToWait : DEFAULT_BLOCKS_TO_WAIT
+  var transactionReceiptAsync = async function (txnHash, resolve, reject) {
+    try {
+      var receipt = web3.eth.getTransactionReceipt(txnHash)
+      if (!receipt) {
+        setTimeout(function () {
+          transactionReceiptAsync(txnHash, resolve, reject)
+        }, interval)
+      } else {
+        if (blocksToWait > 0) {
+          var resolvedReceipt = await receipt
+          if (!resolvedReceipt || !resolvedReceipt.blockNumber) {
+            setTimeout(function () {
+              transactionReceiptAsync(txnHash, resolve, reject)
+            }, interval)
+          } else {
+            try {
+              var block = await web3.eth.getBlock(resolvedReceipt.blockNumber)
+              var current = await web3.eth.getBlock('latest')
+              if (current.number - block.number >= blocksToWait) {
+                var txn = await web3.eth.getTransaction(txnHash)
+                if (txn.blockNumber != null) resolve(resolvedReceipt)
+                else reject(new Error('Transaction with hash: ' + txnHash + ' ended up in an uncle block.'))
+              } else {
+                setTimeout(function () {
+                  transactionReceiptAsync(txnHash, resolve, reject)
+                }, interval)
+              }
+            } catch (e) {
+              setTimeout(function () {
+                transactionReceiptAsync(txnHash, resolve, reject)
+              }, interval)
+            }
+          }
+        } else resolve(receipt)
       }
-    )
-    console.log(`RootChain.startExit(): txhash = ${receipt.transactionHash}`)
+    } catch (e) {
+      reject(e)
+    }
   }
-}
+
+  if (Array.isArray(txnHash)) {
+    var promises = []
+    txnHash.forEach(function (oneTxHash) {
+      promises.push(awaitTx(web3, oneTxHash, options))
+    })
+    return Promise.all(promises)
+  } else {
+    return new Promise(function (resolve, reject) {
+      transactionReceiptAsync(txnHash, resolve, reject)
+    })
+  }
+};
